659e286b510936e9c9fc7423e9d7a0afe354df38
BACHAM MANOJ KUMAR REDDY <2200030709@kluniversity.in>
2024-12-08 04:16:44 +0530

    login.js

--- src/Patient/Login.js
+++ src/Patient/Login.js
@@ -22,7 +22,7 @@ function LoginPage() {
     const patientLoginData = { email, password };
 
     try {
-      const response = await fetch('http://localhost:9999/patientLogin', {
+      const response = await fetch('https://sdp-2200030709-production.up.railway.app/patientLogin', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
