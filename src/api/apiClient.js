48a1283fca86bd48effe40f7e9968b40b22a6340
BACHAM MANOJ KUMAR REDDY <141983293+BachamManoj@users.noreply.github.com>
2025-12-09 16:01:28 +0530

    Update apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:9999",
+    baseURL: "https://sdp-2200030709-production-medicalappointment.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
